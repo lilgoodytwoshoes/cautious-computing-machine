f9dc241d66b911b009a037c450f8b6dcb39f4312
Esmeralda <m.ssd.orrr@gmail.com>
2026-06-11 16:51:47 +0200

    Refactor numeri_perfetti.py to include input prompt within a loop and optimize divisor calculation

--- Liste/numeri_perfetti.py
+++ Liste/numeri_perfetti.py
@@ -1,15 +1,16 @@
-numero = input("Dammi un numero: ")
-numero = int(numero)
-def divisori(numero):
-    divisorioni = []
-    for num in range(1, numero):
-        if numero % num == 0:
-            divisorioni.append(num)
-    return divisorioni
+while True:
+    numero =int(input("Dammi un numero: "))
+    def divisori(numero):
+        divisorioni = []
+        metà_numero=int(numero/2)+1
+        for num in range(1,metà_numero):
+            if numero % num == 0:
+                divisorioni.append(num)
+        return divisorioni
 
-divisorioni = divisori(numero)
-somma = sum(divisorioni)
-if somma == numero:
-    print("Perfetto!")
-else:
-    print("Non perfetto.")
+    divisorioni = divisori(numero)
+    somma = sum(divisorioni)
+    if somma == numero:
+        print("Perfetto!")
+    else:
+        print("Non perfetto.")
